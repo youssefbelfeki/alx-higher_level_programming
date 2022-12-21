b43669755f371cdd32d9defecae360bbef32ecc8
youssefbelfeki <youssef.belfeki@gmail.com>
2022-12-21 09:29:19 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
         """Initialize a new square.
 
         Args:
-        size (int): The size of the new square.
+             size (int): The size of the new square.
         """
         self.size = size
     @property
