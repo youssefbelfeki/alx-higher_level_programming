9da6d8e7adf67aa89af6efe26ad4c8627a9a1ea3
youssefbelfeki <youssef.belfeki@gmail.com>
2022-12-21 09:26:13 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,12 +13,10 @@ class Square:
         size (int): The size of the new square.
         """
         self.size = size
-    
     @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -26,7 +24,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
