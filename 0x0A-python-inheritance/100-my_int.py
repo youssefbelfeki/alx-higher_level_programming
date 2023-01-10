0bf5f3c6b22f43f3452411ffed8dd6d8f20b973e
youssefbelfeki <98311670+youssefbelfeki@users.noreply.github.com>
2023-01-10 21:04:20 +0100

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+''' Module: 1-my_list
+'''
 
 
-class MyInt(int):
-    """Invert int operators == and !=."""
+class MyList(list):
+    ''' Represents a MyList
+    '''
 
-    def __eq__(self, value):
-        """Override == opeartor with != behavior."""
-        return self.real != value
-
-    def __ne__(self, value):
-        """Override != operator with == behavior."""
-        return self.real == value
+    def print_sorted(self):
+        '''
+        prints the list, but sorted
+        '''
+        print(sorted(self))
