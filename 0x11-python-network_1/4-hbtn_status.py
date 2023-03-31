1d599a0cea24c00651138950bcae04f286c9ba34
youssefbelfeki <98311670+youssefbelfeki@users.noreply.github.com>
2023-03-31 10:05:01 +0100

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+Module 4-hbtn-status
+Fetches https://intranet.hbtn.io/status
+"""
+
+
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
