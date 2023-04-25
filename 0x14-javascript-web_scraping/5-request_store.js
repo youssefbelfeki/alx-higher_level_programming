a170e66346472699ea4efd096439e2b74336033a
youssefbelfeki <98311670+youssefbelfeki@users.noreply.github.com>
2023-04-25 13:44:48 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 const url = process.argv[2];
-const filePath = process.argv[3];
+const path = process.argv[3];
 
-request(url, function (err, response, body) {
+request(url, function (err, data, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    fs.writeFile(path, body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
